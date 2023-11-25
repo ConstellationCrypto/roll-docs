1a6a591d149e458a852352622b2b71bf044d29b3
AlexGu <gu.alex.88@gmail.com>
2023-11-24 19:42:26 -0500

    Fill in Roll content

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,19 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
+import roll from '../images/roll.png'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
-  project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
-  },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
-  footer: {
-    text: 'Nextra Docs Template',
-  },
+  logo: <span>Roll Documentation</span>,
+  // project: {
+  //   link: 'https://github.com/shuding/nextra-docs-template',
+  // },
+  // chat: {
+  //   link: 'https://discord.com',
+  // },
+  // docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  // footer: {
+  //   text: 'Nextra Docs Template',
+  // },
 }
 
 export default config
